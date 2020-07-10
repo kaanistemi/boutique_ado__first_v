c7bcdb3ed35cd9dfdd31711b2a26c8d94054095a
kaanistemi <kaanistemi1@gmail.com>
2020-07-10 14:21:32 +0000

    customize admin page

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,5 +1,4 @@
 """boutique_ado URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -21,7 +20,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = {
+
+urlpatterns = [
     path('', views.index, name='home')
-}
-
-
+]
