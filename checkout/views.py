126cd10fc5e24267ad9aa2ace9285beded3869c8
kaanistemi <kaanistemi1@gmail.com>
2020-07-16 13:15:31 +0000

    adding stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51H5WPwKRJqxFEWGZnkHYnHojgXzkdzfM63c9u4JTMDY8ooCqzJO4s0neVawKYCaJH46plOURgEyZphc2o5Hq8XXy00UGGrAmHN',
+        'client_secret': 'test client secret',
+        
     }
 
     return render(request, template, context)
